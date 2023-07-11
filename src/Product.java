02117fc81326face37d7df2b5082553695a742bc
pauloeufrasio <henriquepaulo0606@gmail.com>
2023-07-11 19:49:49 -0300

    Sobrecarga de construtores

--- src/Product.java
+++ src/Product.java
@@ -2,6 +2,8 @@ public class Product {
     public String name;
     public double price;
     public int quantity;
+    public Product(){ // sobrecarga construtor padrão
+    }
 
     public Product(String name, double price, int quantity) {
         this.name = name;
@@ -9,9 +11,9 @@ public class Product {
         this.quantity = quantity;
     }
 
-    public Product(String name, int quantity) { // Construtor sobrecarregado
+    public Product(String name,double price) { // Construtor sobre carregado
         this.name = name;
-        this.quantity = quantity;
+        this.price = price;
     }
 
     public double totalValueInStock() {
