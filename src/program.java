c26d51103ad4112aac8268465bd0305116bb2808
pauloeufrasio <henriquepaulo0606@gmail.com>
2023-07-10 23:27:34 -0300

    Initial commit

---
+++ src/program.java
@@ -0,0 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Product product = new Product(); //chamar classe de métodos e atributos
+
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        product.name = sc.nextLine();
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+        System.out.println();
+        System.out.println("Product data: " + product);
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt(); // variavel local pra entrada do codigo
+
+        product.addProducts(quantity);  // método adicionar produto
+        System.out.println();
+        System.out.println("Updated data: " + product);  //
+        System.out.println();
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity); // método remover produto
+        System.out.println();
+        System.out.println("Updated data: " + product);
+    }
+}
